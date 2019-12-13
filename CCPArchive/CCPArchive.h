0425442f9a6e8526faa53403475c169d72f8f081
Brian.Chu <chuchengpeng0119@163.com>
2019-12-13 17:49:34 +0800

    Initial Commit

---
+++ CCPArchive/CCPArchive.h
@@ -0,0 +1,19 @@
+//
+//  CCPArchive.h
+//  CCPArchive
+//
+//  Created by 储诚鹏 on 2019/12/13.
+//  Copyright © 2019 储诚鹏. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CCPArchive.
+FOUNDATION_EXPORT double CCPArchiveVersionNumber;
+
+//! Project version string for CCPArchive.
+FOUNDATION_EXPORT const unsigned char CCPArchiveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CCPArchive/PublicHeader.h>
+
+
